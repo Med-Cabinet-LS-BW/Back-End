5d228de68ea3dbfcf19064a963de173d26114162
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-17 10:48:47 -0800

    adds get /favorites for logged in user

--- users/users.router.js
+++ users/users.router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const { normalizeStrains } = require('../strains/strains.helpers.js')
+const Favorites = require('../favorites/favorites.model.js')
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: `users router is up` });
 });
+
+router.get('/favorites', async (req, res) => {
+  const { id } = req.docodedToken
+  try {
+    const favorites = normalizeStrains(await Favorites.findByUserId(id));
+    res.status(200).json(favorites)
+
+  } catch (error) {
+    res.status(500).json({ message: `error retrieving favorites from the db`, error })
+  }
+})
 
 module.exports = router;
