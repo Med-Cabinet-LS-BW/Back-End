e4eb1cbc91d2d1ea65339f56977b76181d87012e
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-18 13:22:47 -0800

    wires up users model and auth router to server

--- api/api.router.js
+++ api/api.router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const authRouter = require('../auth/auth.router.js');
+
 router.get('/', (req, res) => {
   res.status(200).json({ message: `api is up` });
 });
+
+router.use('/auth', authRouter);
 
 module.exports = router;
