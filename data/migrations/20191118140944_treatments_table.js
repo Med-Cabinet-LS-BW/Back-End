448d2fc9693c0d555addd1d9b57377d3d7d45820
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-18 14:32:00 -0800

    creates treatments schema and table.

---
+++ data/migrations/20191118140944_treatments_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function (knex) {
+  return knex.schema.createTable('treatments', treatments => {
+    treatments.increments();
+
+    treatments.string('strain', 128).notNullable();
+    treatments.string('method', 255).notNullable();
+    treatments.string('dosage', 255).notNullable();
+    treatments.string('schedule', 255).notNullable();
+    treatments.string('symptoms', 500).notNullable();
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('treatments')
+};
