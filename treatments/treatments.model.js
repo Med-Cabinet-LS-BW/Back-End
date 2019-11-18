e36549ef30c4408f7c704378620cf72ddd28ee27
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-18 15:18:42 -0800

    adds update treatment

--- treatments/treatments.model.js
+++ treatments/treatments.model.js
@@ -18,3 +18,14 @@ async function add(treatment) {
   const [id] = await db('treatments').insert(treatment, 'id');
   return findByTreatmentId(id).first();
 }
+
+async function update(id, fields) {
+  try {
+    const updated = await db('treatments').where({ id }).update(fields, 'id');
+    if (updated) {
+      return findByTreatmentId(id).first();
+    }
+  } catch (error) {
+    return error
+  }
+}
