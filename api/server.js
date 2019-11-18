fe093e9789a7866573c3756b11cb24d4b297b39c
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-17 17:58:50 -0800

    scaffolds middleware, server, and combined router

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const configureMiddleware = require('./configure.middleware.js');
+const apiRouter = require('./api.router.js');
+
+const server = express();
+
+configureMiddleware(server);
+
+server.use('/api', apiRouter);
+
+module.exports = server;
