edaccaa81d86c4ad228bdd2419ac561d321baf3f
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-19 10:37:27 -0800

    implements GET /strains/:strain_id

--- strains/strains.model.js
+++ strains/strains.model.js
@@ -2,8 +2,13 @@ const db = require('../data/dbConfig.js');
 
 module.exports = {
   findAll,
+  findById,
 };
 
 function findAll() {
   return db('strains');
+}
+
+function findById(strain_id) {
+  return db('strains').where('strain_id', '=', strain_id);
 }
