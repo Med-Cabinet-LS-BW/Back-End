8c16610e2b6e3f40f08b15bca4a4ca018896d154
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-19 10:31:52 -0800

    GET /strains converts string lists into arrays

--- strains/strains.helpers.js
+++ strains/strains.helpers.js
@@ -1,34 +1,34 @@
-const axios = require('axios')
-const url = `https://medizen-ds.herokuapp.com/strains`
+const axios = require('axios');
+const url = `https://medizen-ds.herokuapp.com/strains`;
 
-/**
- * 
-  const effects = new Set()
-  const flavors = new Set()
-  effects.add(...Effects.split(','))
-  flavors.add(...flavors.split(','))
+module.exports = {
+  normalizeStrains,
+};
 
- */
-
-getStrains()
+function normalizeStrains(strains) {
+  return strains.map(strain => ({
+    ...strain,
+    effects: strain.effects.split(','),
+    flavors: strain.flavors.split(','),
+  }));
+}
 
 async function getStrains() {
   try {
-    const strainData =
-      await (await axios.get(url))
-        .data
-        .map(({ index, Strain, Type, Rating, Effects, Flavor, Description }) => {
-          return {
-            strain_id: index,
-            strain: Strain,
-            type: Type,
-            rating: Rating,
-            description: Description
-          }
-        })
-    console.log(strainData)
-    return strainData
+    const strainData = await (await axios.get(url)).data.map(
+      ({ index, Strain, Type, Rating, Effects, Flavor, Description }) => {
+        return {
+          strain_id: index,
+          strain: Strain,
+          type: Type,
+          rating: Rating,
+          description: Description,
+        };
+      }
+    );
+    console.log(strainData);
+    return strainData;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
