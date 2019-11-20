1b5f3db42aae0fde7bafa7f684060e92900fb1dc
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-20 08:35:18 -0800

    makes favorites resource endpoint more clear

--- favorites/favorites.router.js
+++ favorites/favorites.router.js
@@ -1,30 +1,29 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-const Favorites = require('./favorites.model.js')
+const Favorites = require('./favorites.model.js');
 
-const { normalizeStrains } = require('../strains/strains.helpers.js')
-const { validateStrainId } = require('../strains/strains.middleware.js')
+const { normalizeStrains } = require('../strains/strains.helpers.js');
+const { validateStrainId } = require('../strains/strains.middleware.js');
 
-router.get('/', async (req, res) => {
-  const { id } = req.docodedToken
+router.get('/strains', async (req, res) => {
+  const { id } = req.docodedToken;
   try {
     const favorites = normalizeStrains(await Favorites.findByUserId(id));
-    res.status(200).json(favorites)
-
+    res.status(200).json(favorites);
   } catch (error) {
-    res.status(500).json({ message: `error retrieving favorites from the db`, error })
+    res.status(500).json({ message: `error retrieving favorites from the db`, error });
   }
-})
+});
 
-router.post('/', validateStrainId, async (req, res) => {
+router.post('/strains', validateStrainId, async (req, res) => {
   const { strain_id } = req.body;
   const user_id = req.docodedToken.id;
   try {
-    const addedFavorite = await Favorites.add({ strain_id, user_id })
-    res.status(201).json(addedFavorite)
+    const addedFavorite = await Favorites.add({ strain_id, user_id });
+    res.status(201).json(addedFavorite);
   } catch (error) {
-    res.status(500).json({ message: `Error saving favorite to database`, error })
+    res.status(500).json({ message: `Error saving favorite to database`, error });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
