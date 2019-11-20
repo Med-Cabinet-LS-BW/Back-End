b40d7e9e0a4a77baf9a38ac07be7205d9e8901f6
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-20 09:01:12 -0800

    adds DELETE favorites/strains/:favorite_id

--- favorites/favorites.model.js
+++ favorites/favorites.model.js
@@ -1,8 +1,10 @@
 const db = require('../data/dbConfig.js');
+const Strains = require('../strains/strains.model.js');
 
 module.exports = {
   findByUserId,
   add,
+  remove,
 };
 
 function findByUserId(id) {
@@ -10,6 +12,7 @@ function findByUserId(id) {
     .join('strains as s', 's.strain_id', '=', 'user_favorites.strain_id')
     .where('user_favorites.user_id', '=', id)
     .select(
+      'user_favorites.id as favorite_id',
       's.strain_id',
       's.strain',
       's.type',
@@ -22,4 +25,23 @@ function findByUserId(id) {
 
 function add(favorite) {
   return db('user_favorites').insert(favorite, 'id');
+}
+
+async function remove(id, user_id) {
+  try {
+    const strain_id = await db('user_favorites')
+      .where({ id })
+      .select('strain_id')
+      .first();
+    await db('user_favorites')
+      .where({ id, user_id })
+      .del();
+    console.log(strain_id.strain_id);
+    return db('strains')
+      .where(strain_id)
+      .select('*')
+      .first();
+  } catch (error) {
+    return error;
+  }
 }
