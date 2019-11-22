6363f63795dee0a98c42a94cc8c8c357ebc6b532
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-22 10:17:25 -0800

    fixes typo. docodedToken => decodedToken

--- favorites/favorites.router.js
+++ favorites/favorites.router.js
@@ -6,7 +6,7 @@ const { normalizeStrains } = require('../strains/strains.helpers.js');
 const { validateStrainId } = require('../strains/strains.middleware.js');
 
 router.get('/strains', async (req, res) => {
-  const { id } = req.docodedToken;
+  const { id } = req.decodedToken;
   try {
     let favorites = await Favorites.findByUserId(id);
     if (favorites.length > 0) {
@@ -21,7 +21,7 @@ router.get('/strains', async (req, res) => {
 
 router.post('/strains', validateStrainId, async (req, res) => {
   const { strain_id } = req.body;
-  const user_id = req.docodedToken.id;
+  const user_id = req.decodedToken.id;
   try {
     const addedFavorite = await Favorites.add({ strain_id, user_id });
     res.status(201).json(addedFavorite);
@@ -32,7 +32,7 @@ router.post('/strains', validateStrainId, async (req, res) => {
 
 router.delete('/strains/:favorite_id', async (req, res) => {
   const { favorite_id } = req.params;
-  const user_id = req.docodedToken.id;
+  const user_id = req.decodedToken.id;
   try {
     const removedFavorite = await Favorites.remove(favorite_id, user_id);
     res.status(200).json(normalizeStrains([removedFavorite]));
--- strains/strains.model.js
+++ strains/strains.model.js
@@ -22,6 +22,7 @@ const strainFields = [
 function find(limit, offset) {
   limit = limit || 20;
   offset = offset || 0;
+
   return db('strains')
     .orderBy('strain_id')
     .limit(limit)
