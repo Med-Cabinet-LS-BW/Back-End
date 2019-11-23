b82a6df5e7a88b166df043be871367de11f06096
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-22 16:54:26 -0800

    Revert "refactors DELETE favorite"
    This reverts commit 813ed7af39e2326da1ca31c095753b1f0ec120c5.

--- favorites/favorites.middleware.js
+++ favorites/favorites.middleware.js
@@ -2,7 +2,6 @@ const Favorites = require('../favorites/favorites.model.js');
 
 module.exports = {
   findFavoriteStrainIds,
-  validateFavoriteId,
 };
 
 async function findFavoriteStrainIds(req, res, next) {
@@ -12,19 +11,5 @@ async function findFavoriteStrainIds(req, res, next) {
     next();
   } catch (error) {
     res.status(500).json({ message: `Error retrieving favorites for user_id:${user_id}`, error });
-  }
-}
-
-async function validateFavoriteId(req, res, next) {
-  const { favorite_id } = req.params;
-  try {
-    const favorite = await Favorites.findBy({ id: favorite_id });
-    if (favorite) {
-      next();
-    } else {
-      res.status(404).json({ message: `Favorite with id ${favorite_id} not found` });
-    }
-  } catch (error) {
-    res.status(500).json({ message: `error validating favorite id`, error: error.message });
   }
 }
