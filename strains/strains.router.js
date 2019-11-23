992483cde98b4d7bb745cada5cd3e5d97cad6b5e
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-22 16:54:26 -0800

    Revert "clearer error message for recommendations"
    This reverts commit ebbd6f0c3c2cb2e3d04198e06168463b33554d23.

--- strains/strains.router.js
+++ strains/strains.router.js
@@ -65,21 +65,25 @@ router.post('/', async (req, res) => {
 router.post('/recommendations', findFavoriteStrainIds, async (req, res) => {
   // middleware candidate
   const { filters } = req.body;
+  const { id } = req.decodedToken;
   const { favorites } = req;
 
   let limit = req.body.limit || 10;
   if (!filters) {
-    res.status(400).json({ message: `filters are required in order to receive recommendations` });
+    res.status(400).json({ message: `filters are required.` });
   } else if (!filters.length) {
     res.status(400).json({
       message: `Received filters of type ${typeof filters}. Filters must be of type array.`,
     });
   } else {
     try {
+      // needs better error handling here
+      // lots of requests happening
+
       const filterString = filters.join('%2C');
       const url = `https://medizen-ds.herokuapp.com/rec/${limit}/${filterString}`;
-      const { data: strainIds } = await axios.get(url);
 
+      const { data: strainIds } = await axios.get(url);
       const strains = await Strains.findByIds(strainIds).map(async s => {
         return {
           ...s,
@@ -89,7 +93,8 @@ router.post('/recommendations', findFavoriteStrainIds, async (req, res) => {
 
       res.status(200).json(normalizeStrains(strains));
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      console.log(error);
+      res.status(500).json({ message: `Could not GET from ds api` });
     }
   }
 });
